7ef6df17508c951af3d4f0dfb64f439d4e1ad460
Anthony <ajackerm@mtu.edu>
2025-01-24 18:12:02 -0500

    Legacy Update

--- Finance/monthly_bills/views/legacy_portal_view.py
+++ Finance/monthly_bills/views/legacy_portal_view.py
@@ -106,9 +106,6 @@ def expense_overview(request):
     }
 
     writeoffs = WriteOff.objects.all().order_by('-date')
-    for x in writeoffs:
-        x.added_by = UserProfile.objects.get(user__first_name="Joyce")
-        x.save()
 
     if request.method == 'POST':
         copy_request = request.POST.copy()
