cfc015c9233ad395195fac2401b6249a9dc92d0a
Anthony <ajackerm@mtu.edu>
2025-03-24 13:59:56 -0400

    Fixed the login bug

--- Finance/budgeting/signals.py
+++ Finance/budgeting/signals.py
@@ -10,4 +10,4 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    instance.profile.save()
